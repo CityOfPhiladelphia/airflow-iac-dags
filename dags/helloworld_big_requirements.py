305d2024f78876c04c146d71794384eb29cf7d98
Ryan Weast <ryweast@gmail.com>
2025-09-30 14:15:50 -0400

    Add sample big job

---
+++ dags/helloworld_big_requirements.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import time
+
+
+def wait_a_while():
+    time.sleep(60)
+
+
+def hello_world():
+    print("Hello from KubernetesExecutor!")
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2023, 1, 1),
+}
+
+with DAG(
+    dag_id="hello_big_kubernetes_executor",
+    default_args=default_args,
+    schedule=None,
+    catchup=False,
+    tags=["example"],
+) as dag:
+    sleep_task = PythonOperator(task_id="wait_a_while", python_callable=wait_a_while)
+    hello_task = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello_world,
+        executor_config={
+            "KubernetesExecutor": {
+                "request_memory": "4Gi",
+                "limit_memory": "4Gi",
+                "request_cpu": "6",
+            }
+        },
+    )
