ddf173c5d51fd93082d224146e9c758c030702a6
Ryan Weast <ryweast@gmail.com>
2025-10-01 14:51:42 -0400

    Add sleep

--- dags/databridge_etl_tools_test.py
+++ dags/databridge_etl_tools_test.py
@@ -41,6 +41,7 @@ with DAG(
 ) as dag:
     bash_operator = BashOperator(
         task_id="databridge_etl_tools_help",
-        bash_command=f'echo "{databridge_test_conn_string}"; databridge_etl_tools --help',
+        bash_command="sleep 10; databridge_etl_tools --help",
+        # bash_command=f'echo "{databridge_test_conn_string}"; databridge_etl_tools --help',
         executor_config=k8s_exec_config_custom_image,
     )
