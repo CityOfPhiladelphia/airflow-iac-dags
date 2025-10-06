eef8f9f2985b3d71153489147ca8b59fd7d14689
Ryan Weast <ryweast@gmail.com>
2025-10-06 16:22:17 -0400

    Use different dags instead of different tasks

--- dags/helloworld_small_requirements.py
+++ dags/helloworld_small_requirements.py
@@ -6,10 +6,6 @@ from kubernetes.client import models as k8s
 import time
 
 
-def wait_a_while():
-    time.sleep(10)
-
-
 def hello_world():
     print("Hello from KubernetesExecutor!")
 
@@ -19,15 +15,15 @@ default_args = {
     "start_date": datetime(2023, 1, 1),
 }
 
-with DAG(
-    dag_id="hello_small_kubernetes_executor",
-    default_args=default_args,
-    schedule=None,
-    catchup=False,
-    tags=["example"],
-) as dag:
-    for i in range(15):
-        cpu_amt = (i + 2) * 15
+for i in range(10):
+    cpu_amt = 100 + i * 20
+    with DAG(
+        dag_id=f"hello_world_{cpu_amt}",
+        default_args=default_args,
+        schedule=None,
+        catchup=False,
+        tags=["example"],
+    ) as dag:
         k8s_exec_config_resource_requirements = {
             "pod_override": k8s.V1Pod(
                 spec=k8s.V1PodSpec(
@@ -59,3 +55,44 @@ with DAG(
             bash_command="echo hello world",
             executor_config=k8s_exec_config_resource_requirements,
         )
+
+# with DAG(
+#    dag_id="hello_small_kubernetes_executor",
+#    default_args=default_args,
+#    schedule=None,
+#    catchup=False,
+#    tags=["example"],
+# ) as dag:
+#    for i in range(15):
+#        cpu_amt = (i + 2) * 15
+#        k8s_exec_config_resource_requirements = {
+#            "pod_override": k8s.V1Pod(
+#                spec=k8s.V1PodSpec(
+#                    containers=[
+#                        k8s.V1Container(
+#                            name="base",
+#                            resources=k8s.V1ResourceRequirements(
+#                                requests={
+#                                    "cpu": f"{cpu_amt}m",
+#                                    "memory": "256Mi",
+#                                },
+#                                limits={
+#                                    "cpu": f"{cpu_amt}m",
+#                                    "memory": "256Mi",
+#                                },
+#                            ),
+#                        )
+#                    ]
+#                )
+#            )
+#        }
+#        hello_task = PythonOperator(
+#            task_id=f"say_hello_world_python_small_{cpu_amt}",
+#            python_callable=hello_world,
+#            executor_config=k8s_exec_config_resource_requirements,
+#        )
+#        bash_operator = BashOperator(
+#            task_id=f"say_hello_world_bash_small_{cpu_amt}",
+#            bash_command="echo hello world",
+#            executor_config=k8s_exec_config_resource_requirements,
+#        )
