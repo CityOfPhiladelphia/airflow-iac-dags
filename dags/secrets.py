6d0f40b5d9d4c690674cccb7b775dbad9c6fd254
Ryan Weast <ryweast@gmail.com>
2025-10-16 10:00:33 -0400

    Try using Python instead

--- dags/secrets.py
+++ dags/secrets.py
@@ -1,14 +1,33 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 from pytz import timezone
 from airflow.models import Connection
+from airflow import settings
 import json
 
 default_args = {
     "owner": "airflow",
     "start_date": datetime.now(timezone("US/Eastern")),
 }
+
+
+def add_connection():
+    conn = Connection(
+        conn_id="test-abcd",
+        conn_type="http",
+        host="phila.gov",
+        login="admin",
+        password="adminpass",
+        schema="test-schema",
+        port="443",
+        extra={"abcd": "defg"},
+    )
+    session = settings.Session()
+    session.add(conn)
+    session.commit()
+
 
 # https://airflow.apache.org/docs/apache-airflow/stable/howto/connection.html#creating-a-connection-from-the-cli
 with DAG(
@@ -20,23 +39,7 @@ with DAG(
     # Tags are useful for filtering
     tags=["secrets"],
 ) as dag:
-    conn = Connection(
-        conn_id="test-abcd",
-        conn_type="http",
-        host="phila.gov",
-        login="admin",
-        password="adminpass",
-        schema="test-schema",
-        port="443",
-        extra={"abcd": "defg"},
+    PythonOperator(
+        task_id="add_test_connection",
+        python_callable=add_connection,  # This refers to `def hello_world()`
     )
-    command = [
-        "airflow",
-        "connections",
-        "add",
-        "'test-add-connection'",
-        "--conn-json",
-        f"'{json.dumps(conn.to_dict())}'",
-    ]
-
-    BashOperator(task_id="add_test_connection", bash_command=" ".join(command))
